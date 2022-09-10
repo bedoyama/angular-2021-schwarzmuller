1d6cee0663a6b0ce217fc3bf574d0b2f0645166f
mauricio <bedoyama@gmail.com>
2022-09-10 17:29:56 -0500

    Adding recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 
+              'Test', 
+              'https://www.foodandwine.com/thmb/oir67NBP66GIcb8rexePDB3Fx_g=/650x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/jamaican-jerk-chicken-FT-RECIPE0918-eabbd55da31f4fa9b74367ef47464351.jpg')
+  ];
 
   constructor() { }
 
